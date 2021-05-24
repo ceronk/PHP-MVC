ef90a328f29379e271e7a0e0d1772af5a9f35c3a
Kevin Cerón <kevin10595@hotmail.com>
2021-05-24 02:08:47 -0600

    Create nota.php

---
+++ MODELS/nota.php
@@ -0,0 +1,29 @@
+<?php
+
+class Nota extends ModeloBase{
+    public $nombre, $contenido;
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+ 
+    public function getContenido()
+    {
+        return $this->contenido;
+    }
+ 
+    public function setContenido($contenido)
+    {
+        $this->contenido = $contenido;
+
+        return $this;
+    }
+}
