df16c7f41fa3da7bad313610faa01e06144510fc
Kevin Cerón <kevin10595@hotmail.com>
2021-05-24 01:13:44 -0600

    Create usuario.php

---
+++ CONTROLLERS/usuario.php
@@ -0,0 +1,18 @@
+<?php
+
+//El controlador encapsula acciones
+class UsuarioController{
+    
+    
+    public function mostrarTodos(){
+
+        require_once '../MODELS/usuario.php'; //cargando modelo
+    }
+
+
+    public function crearUsuario(){
+
+    }
+
+
+}
