8b8b7c52d96018d466ad601aff5c11f1b38e288f
Kevin Cerón <kevin10595@hotmail.com>
2021-05-24 01:13:46 -0600

    Create usuario.php

---
+++ MODELS/usuario.php
@@ -0,0 +1,70 @@
+<?php
+
+
+class Usuario{
+    public $nombre, $apellidos, $email, $pass;
+
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+  
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+  
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+  
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+  
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPass()
+    {
+        return $this->pass;
+    }
+
+    public function setPass($pass)
+    {
+        $this->pass = $pass;
+
+        return $this;
+    }
+
+
+    public function conseguirTodos(){
+        
+    }
+
+
+
+
+
+}
