e0275e6b30b28156897cf93750722c24ed099b2b
Kevin Cerón <kevin10595@hotmail.com>
2021-05-24 02:00:07 -0600

    Update usuario.php

--- CONTROLLERS/usuario.php
+++ CONTROLLERS/usuario.php
@@ -1,18 +1,25 @@
 <?php
 
 //El controlador encapsula acciones
-class UsuarioController{
-    
-    
-    public function mostrarTodos(){
+class UsuarioController
+{
 
-        require_once '../MODELS/usuario.php'; //cargando modelo
+
+    public function mostrarTodos()
+    {
+
+        require_once 'MODELS/usuario.php'; //cargando modelo
+
+        $usuario = new Usuario();
+
+        $allUsers = $usuario->mostrarTodos();
+
+        require_once 'VIEWS/USUARIOS/mostrarTodos.php'; //cargando vista
     }
 
 
-    public function crearUsuario(){
-
+    public function crearUsuario()
+    {
+        require_once 'VIEWS/USUARIOS/crearUsuario.php';
     }
-
-
 }
