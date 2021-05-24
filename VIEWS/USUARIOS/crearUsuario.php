a8341e3633dfab8024fff93921620fe666ae09e1
Kevin Cerón <kevin10595@hotmail.com>
2021-05-24 02:00:13 -0600

    Create crearUsuario.php

---
+++ VIEWS/USUARIOS/crearUsuario.php
@@ -0,0 +1,15 @@
+<form action="" method="post">
+
+<label for="">Nombre</label>
+<input type="text" name="" id="">
+
+<label for="">Apellidos</label>
+<input type="text" name="" id="">
+
+<label for="">Email</label>
+<input type="text" name="" id="">
+
+<label for="">Pass</label>
+<input type="text" name="" id="">
+
+</form>
